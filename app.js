f9d2c7dd5dddaedca41ea7c42db36af72d693ec7
ZejunZhou <zzhou443>
2023-02-16 23:40:43 -0600

    updating css and set up static files

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const bodyParser = require("body-parser");
 /**  Use express body-parser module **/
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
+/**use static file */
+app.use(express.static('public'));
 
 app.listen("3000", ()=>{
     console.log("server is on port 3000");
+})
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/signup.html");
 })
